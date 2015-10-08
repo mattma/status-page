ac96c86b97cf823c3ed7df183b1227ea836785c7
Matt Ma <bigmabig@gmail.com>
2015-10-08 10:18:39 -0700

    update time-format jshint issue, fix the time millisecond convertion

--- app/helpers/time-format.js
+++ app/helpers/time-format.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import format from 'incident/utils/time/format';
 
 export function timeFormat(params/*, hash*/) {
-  const date = new Date(params[0] / 1000);
+  const date = new Date(params[0] / 1000 / 1000);
   const formatString = params[1] || "#MMM# #DD#, #YYYY#";
   return format.custom(date, formatString);
 }
--- app/utils/time/format.js
+++ app/utils/time/format.js
@@ -36,11 +36,11 @@ export default {
     MMM = (MMMM=["January","February","March","April","May","June","July","August","September","October","November","December"][M-1]).substring(0,3);
     DD = (D=date.getDate())<10?('0'+D):D;
     DDD = (DDDD=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][date.getDay()]).substring(0,3);
-    th=(D>=10&&D<=20)?'th':((dMod=D%10)==1)?'st':(dMod==2)?'nd':(dMod==3)?'rd':'th';
+    th=(D>=10&&D<=20)?'th':((dMod=D%10)===1)?'st':(dMod===2)?'nd':(dMod===3)?'rd':'th';
     formatString = formatString.replace("#YYYY#",YYYY).replace("#YY#",YY).replace("#MMMM#",MMMM).replace("#MMM#",MMM).replace("#MM#",MM).replace("#M#",M).replace("#DDDD#",DDDD).replace("#DDD#",DDD).replace("#DD#",DD).replace("#D#",D).replace("#th#",th);
     h=(hhh=date.getHours());
-    if (h==0) h=24;
-    if (h>12) h-=12;
+    if (h===0) {h=24;}
+    if (h>12) {h-=12;}
     hh = h<10?('0'+h):h;
     hhhh = h<10?('0'+hhh):hhh;
     AMPM=(ampm=hhh<12?'am':'pm').toUpperCase();
@@ -48,4 +48,4 @@ export default {
     ss=(s=date.getSeconds())<10?('0'+s):s;
     return formatString.replace("#hhhh#",hhhh).replace("#hhh#",hhh).replace("#hh#",hh).replace("#h#",h).replace("#mm#",mm).replace("#m#",m).replace("#ss#",ss).replace("#s#",s).replace("#ampm#",ampm).replace("#AMPM#",AMPM);
   }
-}
+};
