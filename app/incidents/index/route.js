7a199fdbb2860498fa1e50b2b057de445a46f3fd
Matt Ma <bigmabig@gmail.com>
2015-10-10 13:31:59 -0700

    add a time constant to handle current week timestamp

--- app/incidents/index/route.js
+++ app/incidents/index/route.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
 import ajax from 'incident/utils/ajax/ajax';
+import Time from 'incident/utils/time/constant';
 
 export default Ember.Route.extend({
   model (params) {
+    let today = Time.CURRENT_WEEK();
+    console.log('today: ', today);
     const url = `/incidents/?${params.range}`;
     const opts =  {
       type: "GET",
---
+++ app/utils/time/constant.js
@@ -0,0 +1,73 @@
+// Polyfill to handle `Date.now` is not a method
+(function () {
+  if (!Date.now) {
+    Date.now = function now() {
+      return new Date().getTime();
+    };
+  }
+})();
+
+function _today (date) {
+  return date ? new Date(date) : new Date();
+}
+
+export default {
+  // return current time in millisecond
+  NOW () {
+    return _today();
+  },
+
+  // return 2 digit of current date
+  TODAY () {
+    let D = _today().getDate();
+    D = D < 10 ? ('0'+D) : D;
+    return D;
+  },
+
+  // return 2 digit of current month
+  CURRENT_MONTH () {
+    let M = _today().getMonth() +1;
+    M = M < 10 ? ('0'+M) : M;
+    return M;
+  },
+
+  // return 4 digit year format
+  CURRENT_YEAR () {
+    return _today().getFullYear();
+  },
+
+  /**
+   * Those constants return range value
+   * Unicode range with start and end date
+   */
+
+  /**
+   * Find out the current week
+   * @param {Number} start [description]
+   */
+  CURRENT_WEEK (start=0) {
+    // Calcing the starting point
+    let today_start_timestamp = new Date(_today().setHours(0, 0, 0, 0));
+    let week_start_timestamp = today_start_timestamp.setDate(
+      today_start_timestamp.getDate() - (today_start_timestamp.getDay() - start));
+    let week_end_timestamp = _today().getTime();
+
+    return [week_start_timestamp, week_end_timestamp];
+  },
+
+  PASS_DAY () {
+
+  },
+
+  PASS_WEEK () {
+
+  },
+
+  PASS_MONTH () {
+
+  },
+
+  PASS_YEAR () {
+
+  },
+};
