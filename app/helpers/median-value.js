f3545959a4b3ba3837379ab2cfc95ce46346b0ae
Matt Ma <bigmabig@gmail.com>
2015-10-15 10:51:59 -0700

    showing the graphs with the mean values

---
+++ app/helpers/median-value.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export function medianValue(params/*, hash*/) {
+  let formattedVal;
+  const valueArr = params[1].map(val => val[1]);
+  const meanVal = d3.mean(valueArr);
+
+  switch (params[0]) {
+    case 'up':
+      formattedVal = `${(meanVal * 100).toFixed(1)}%`;
+      break;
+    case 'memory':
+      formattedVal = `${meanVal}ms`;
+      break;
+    default:
+      formattedVal = `${(meanVal * 100).toFixed(1)}%`;
+  }
+
+  return formattedVal;
+}
+
+export default Ember.Helper.helper(medianValue);
