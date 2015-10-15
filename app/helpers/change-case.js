12449da2df7940173505c97750d747256e0fbfb7
Matt Ma <bigmabig@gmail.com>
2015-10-14 20:33:40 -0700

    add a new helper of changeCase instead of className switch helper

---
+++ app/helpers/change-case.js
@@ -0,0 +1,43 @@
+import Ember from 'ember';
+
+/**
+ * Change text into a different format by passing the formatter
+ * @params 0: the text that pass into the function
+ * @params 1: what output you want the text to be
+ * @return String
+ *
+ * ex: {{changeCase "hello-world" "class"}}
+ */
+export function changeCase(params/*, hash*/) {
+  let string;
+
+  switch (params[1]) {
+    case 'lower':
+      string = params[0].toLowerCase();
+      break;
+    case 'upper':
+      string = params[0].toUpperCase();
+      break;
+    case 'camel':
+      string = Ember.String.camelize(params[0]);
+      break;
+    case 'cap':
+      string = Ember.String.capitalize(params[0]);
+      break;
+    case 'class':
+      string = Ember.String.classify(params[0]);
+      break;
+    case 'dash':
+      string = Ember.String.dasherize(params[0]);
+      break;
+    case 'decamel':
+      string = Ember.String.decamelize(params[0]);
+      break;
+    default:
+      string = params[0].toLowerCase();
+  }
+
+  return string;
+}
+
+export default Ember.Helper.helper(changeCase);
---
+++ tests/unit/helpers/change-case-test.js
@@ -0,0 +1,10 @@
+import { changeCase } from '../../../helpers/change-case';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | change case');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = changeCase(42);
+  assert.ok(result);
+});
