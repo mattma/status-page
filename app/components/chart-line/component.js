6aca527ad5a3d0a58ca10dd843ed83b311690fee
Matt Ma <bigmabig@gmail.com>
2015-10-15 12:12:09 -0700

    format the time on x axis

--- app/components/chart-line/component.js
+++ app/components/chart-line/component.js
@@ -7,8 +7,8 @@ function parsedDate(second, chartRange) {
 
   switch (chartRange) {
     case 'pass_day':
-      parseDate = d3.time.format('%H:%M:%S').parse;
-      formattedDate = time.format((second * 1000), "HH:mm:ss");
+      parseDate = d3.time.format('%H:%M').parse;
+      formattedDate = time.format((second * 1000), "HH:mm");
       break;
     case 'pass_week':
       // parseDate = d3.time.format('%H:%M:%S').parse;
@@ -41,9 +41,6 @@ function domain(data, chart) {
   let min = d3.min(data, d => parsedDate(d[0], chartRange));
   let max = d3.max(data, d => parsedDate(d[0], chartRange));
 
-  // console.log('min: ', min);
-  // console.log('max: ', max);
-
   let _w = +(chart.base.attr('width'));
   // Setup xScale domain range
   chart.xScale.domain([min, max]).range([0, _w]);
@@ -58,9 +55,10 @@ d3.chart('Line').extend('StatusLine', {
     const chart = chart || this;
     const chartRange = chart.info.range;
 
+    chart.line.x(d => chart.xScale(parsedDate(d[0], chartRange)));
+
     domain(data, chart);
 
-    chart.line.x(d => chart.xScale(parsedDate(d[0], chartRange)));
     chart.line.y(d => chart.yScale(+(d[1])));
   }
 });
@@ -69,7 +67,7 @@ d3.chart('Axis').extend('MyAxis', {
   onDataBind: function(data){
     var chart = chart || this;
 
-    chart.xAxis.tickPadding(10); //.tickFormat(customTimeFormat);
+    chart.xAxis.tickPadding(10).tickFormat(d3.time.format('%H:%M'));
     chart.xAxis.ticks(5);
     chart.yAxis.ticks(2);
 
@@ -81,11 +79,12 @@ d3.chart("FinalChart", {
   initialize: function(containerInfo) {
     var axis =  this.mixin("MyAxis",  this.base.append("g").classed('axisgroup', true), {
       info: containerInfo,
+      x: 'time',
       guide: true,
       ticksOnResize: true
     });
 
-    axis.xLabel('ms', 0, 20);
+    // axis.xLabel('ms', 0, 20);
 
     this.mixin("StatusLine", this.base.append('g').classed('lines', true), {
       info: containerInfo,
